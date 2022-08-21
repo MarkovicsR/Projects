1a345a8396e91a307b1dc717762c519ce4eb5e8d
Roland <markovicsroland93@gmail.com>
2022-08-21 18:13:49 +0200

    isPalindrome implemented

--- StringTheory/src/StringTheory.java
+++ StringTheory/src/StringTheory.java
@@ -2,14 +2,24 @@ public class StringTheory {
 
     public static void main(String[] args) {
 
-        String sentence = "Nem tudom mi";
-
+        String sentence = "Ro Tato r";
+        System.out.println(isPalindrome(sentence));
 
     }
 
     public static boolean isPalindrome (String stringToCheck) {
-        String
-        char[] charArray = stringToCheck.toCharArray();
+        stringToCheck.replace(" ", "");
+        String input = stringToCheck.toLowerCase();
+        char[] charArray = input.toCharArray();
+        boolean isPalindrome = false;
 
+        for(int i = 0; i < input.length(); i++) {
+            if (charArray[i] == charArray[charArray.length-i -1]) {
+                isPalindrome = true;
+            } else {
+                isPalindrome = false;
+            }
+        }
+        return isPalindrome;
     }
 }
