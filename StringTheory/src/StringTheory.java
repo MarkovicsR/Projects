39bf90f567a5a635c5600023d5817b09783f86ac
Roland <markovicsroland93@gmail.com>
2022-08-21 19:25:43 +0200

    isPangram started

--- StringTheory/src/StringTheory.java
+++ StringTheory/src/StringTheory.java
@@ -2,9 +2,10 @@ public class StringTheory {
 
     public static void main(String[] args) {
 
-        String sentence = "subdermatoglyphic";
+        String sentence = "Sphinx of black quartz judge y vow";
         System.out.println(isPalindrome(sentence));
         System.out.println(isIsogram(sentence));
+        System.out.println(isPangram(sentence));
     }
 
     public static boolean isPalindrome (String stringToCheck) {
@@ -40,6 +41,26 @@ public class StringTheory {
         }
         return isIsogram;
     }
+
+    public static boolean isPangram (String stringToCheck) {
+        char[] charArray = convertToCharArray(stringToCheck);
+        char[] alphabet = new char[] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+        boolean isPangram = true;
+        for (int i = 0; i < alphabet.length; i++) {
+            label:
+            for (int j = 0; j < charArray.length; j++) {
+                if (alphabet[i] == charArray[j]) {
+                    isPangram = true;
+                    break label;
+                } else {
+                    isPangram = false;
+
+                }
+            }
+        }
+        return isPangram;
+    }
+    
 
     public static char[] convertToCharArray (String stringToConvert) {
         String input = stringToConvert.toLowerCase().replace(" ", "");
